2c2b7f9a857f4d51398e90e48d99cea837845f46
Natalie Pateman <nataliempateman@gmail.com>
2016-09-16 11:07:28 +0100

    final project september 2016 not working version

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-//if (cardTwo === cardFour) {
-//	alert("You found a match")
-//}
-//else {
-//	alert("Sorry, try again")
-//}
+var cards = ["queen", "queen", "king", "king"];
 
+var cardsInPlay = [];
 
-var createBoard = function(){
-	var oldBoard = document.getElementById("game-board");
-  
-	for(var i = 0; i < 4; i++) {
+function createBoard() {
       
-      var newCards = document.createElement('div');
-      newCards.className = "card";
-      oldBoard.appendChild(newCards);
-	}
-};
+  var gameBoard = document.getElementById("game-board");  
+  
+  for(var i = 0; i < cards.length; i++) {
+
+      var cardElement = document.createElement("div");
+      cardElement.className = "card";
+      gameBoard.appendChild(cardElement);
+     
+      cardElement.setAttribute("data-card", cards[i]); 
+    
+      cardElement.addEventListener("click", isTwoCards); {
+
+    if (cardElement.getAttribute("data-card") === "queen"){
+cardElement.innerHTML = '<img src="http://previews.123rf.com/images/chrisdorney/chrisdorney1109/chrisdorney110900018/10520647-The-Queen-of-Hearts-Playing-Card--Stock-Photo.jpg" alt="Queen of Hearts"/>';
+}
+    else if (cardElement.getAttribute("data-card")  === "king") {
+cardElement.innerHTML = '<img src="http://static.giantbomb.com/uploads/scale_super/0/1899/2248129-kspreview.png" alt="King of Spades"/>';
+}    
+}
+} 
+} 
+ function isTwoCards() {
+  cardsInPlay.push(this.getAttribute("data-card"));
+  
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }    
+ } 
+function isMatch() {
+
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  }
+    else if (cards[2] === cards[3]) {
+        alert("You found a match!");
+    }
+  else {
+    alert("Try again"); 
+}
+document.getElementById("game-board").innerHTML = "";
+}  
 createBoard();
+
+
